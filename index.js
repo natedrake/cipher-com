03212c83d653d1bdddde1b9da0ba5e9d9415375f
natedrake <john1o1grady@hotmail.com>
2016-09-25 15:40:43 +0100

    Project Initiation

---
+++ index.js
@@ -0,0 +1,23 @@
+var $express = require('express');
+var $app = $express();
+
+
+
+$app.use($express.static(__dirname+'/public'));
+$app.set('views', __dirname+'/public/views');
+$app.set('view engine', 'jade');
+
+/**
+ *  @note GET requests
+ **/
+
+$app.get('/', function($request, $response) {
+    $response.render('index');
+})
+
+
+var $server = $app.listen((process.env.PORT || 8080), function() {
+    var $hostname = $server.address().address;
+    var $port = $server.address().port;
+    console.log("App listening at http://%s:%s", $hostname, $port);
+})
