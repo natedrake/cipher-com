0f9f4cf7a0b3ddbd7c7f67ce2ef3da92277c5b3b
natedrake <john1o1grady@hotmail.com>
2016-09-25 16:25:11 +0100

    Change to port 80

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ $app.get('/', function($request, $response) {
     $response.render('index');
 });
 
-var $server = $app.listen((process.env.PORT || 8080), function() {
+var $server = $app.listen((process.env.PORT || 80), function() {
     var $hostname = $server.address().address;
     var $port = $server.address().port;
     console.log("App listening at http://%s:%s", $hostname, $port);
